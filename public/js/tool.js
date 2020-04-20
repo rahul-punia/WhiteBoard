79c2ec4bc012ac9893bb02f9890d421bebd64428
Akshay Puniya <akshaypuniya@Akshays-MacBook-Air.local>
2020-04-20 15:36:25 +0530

    tool update

--- public/js/tool.js
+++ public/js/tool.js
@@ -1,5 +1,5 @@
 // connect to socket server
-const socket = io.connect("https://dashboard.heroku.com/apps/whiteboardnew/");
+const socket = io.connect("https://dashboard.heroku.com/");//https://pepboard.herokuapp.com/");
 // *********************************Basic Setup
 const board = document.querySelector(".board");
 board.height = window.innerHeight;
