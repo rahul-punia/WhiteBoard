1dd1c45ac29995f63eb952a33c3997ad9690d67f
rahul-punia <rahulpunia1011@gmail.com>
2021-07-21 03:04:42 +0530

    chat Feature completed

--- public/js/tool.js
+++ public/js/tool.js
@@ -1,5 +1,5 @@
 // connect to socket server
-const socket = io.connect("https://whiteboardnew.herokuapp.com/");
+const socket = io.connect("https://pepboard.herokuapp.com/");
 // *********************************Basic Setup
 const board = document.querySelector(".board");
 board.height = window.innerHeight;
--- server.js
+++ server.js
@@ -37,8 +37,72 @@ io.on("connection", function(socket) {
     socket.broadcast.emit("onredo");
   });
 });
-// nodejs server my
+
+
+const formatMessage = require('./utils/messages');
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers
+} = require('./utils/users');
+
+const botName = 'Chat Bot ';
+
+// Run when client connects
+io.on('connection', socket => {
+  socket.on('joinRoom', ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
+
+    socket.join(user.room);
+
+    // Welcome current user
+    socket.emit('message', formatMessage(botName, 'Welcome to Chat Section!'));
+
+    // Broadcast when a user connects
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        'message',
+        formatMessage(botName, `${user.username} has joined the chat`)
+      );
+
+    // Send users and room info
+    io.to(user.room).emit('roomUsers', {
+      room: user.room,
+      users: getRoomUsers(user.room)
+    });
+  });
+
+  // Listen for chatMessage
+  socket.on('chatMessage', msg => {
+    const user = getCurrentUser(socket.id);
+
+    io.to(user.room).emit('message', formatMessage(user.username, msg));
+  });
+
+  // Runs when client disconnects
+  socket.on('disconnect', () => {
+    const user = userLeave(socket.id);
+
+    if (user) {
+      io.to(user.room).emit(
+        'message',
+        formatMessage(botName, `${user.username} has left the chat`)
+      );
+
+      // Send users and room info
+      io.to(user.room).emit('roomUsers', {
+        room: user.room,
+        users: getRoomUsers(user.room)
+      });
+    }
+  });
+});
+
+
+// nodejs server
 const port = process.env.PORT || 3000;
 server.listen(port, function(req, res) {
-  console.log("Server has started at port "+port);
+  console.log("Server has started at port 3000");
 });
